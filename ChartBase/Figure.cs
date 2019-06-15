e7d1dd46a9b8debbbef6583b912f8c8e99f11fb2
manuel meisen <manuel.meisen@gmx.de>
2019-06-15 22:15:21 +0200

    Made Figure a valid option for binding

--- ChartBase/Figure.cs
+++ ChartBase/Figure.cs
@@ -3,15 +3,62 @@ using System.Drawing;
 
 namespace SimpleXart
 {
-    public class Figure
+    public class Figure : INotifyPropertyChanged
     {
-        public float Value { get; set; }
-        public string Describtion { get; set; } = "";
-        public Color Color { get; set; } = Color.LightGray;
 
-        public Figure(float value)
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        private float _value;
+        public float Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                PropertyChanged?.Invoke(this,new PropertyChangedEventArgs("Value"));
+            }
+        }
+
+
+        private string _describtion;
+        public string Describtion
+        {
+            get
+            {
+                return _describtion;
+            }
+            set
+            {
+                _describtion = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Describtion"));
+            }
+        }
+
+        private Color _color;
+        public Color Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Color"));
+            }
+        }
+
+        public Figure(float value, string describtion = "")
         {
             Value = value;
+            Describtion = describtion;
         }
+
+
     }
 }
