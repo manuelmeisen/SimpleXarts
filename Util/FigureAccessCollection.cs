f1b849295342e4c05e0065aa02acbfce2d9d0ef5
Johann <abc@123.de>
2019-06-15 16:54:11 +0200

    FigureAccessCollection moved

---
+++ Util/FigureAccessCollection.cs
@@ -0,0 +1,13 @@
+﻿using SimpleXart.ChartBase;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace SimpleXart
+{
+    internal class FigureAccessCollection : KeyedCollection<object, FigureAccess>
+    {
+        protected override object GetKeyForItem(FigureAccess item) => item.Source;
+    }
+}
